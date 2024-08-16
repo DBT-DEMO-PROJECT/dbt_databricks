3013ae0865c6f042a54afceb5278d20639158b0f
Yethu Krishnan <yethukichu55@gmail.com>
2024-08-16 10:26:50 +0000

    custom schema

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -7,7 +7,7 @@
     {{ 'gold' }}  {# In production, use only 'gold' schema #}
   {%- elif custom_schema_name == 'silver' -%}
     {{ target_schema }}_silver  {# In non-production environments, combine target schema with 'silver' #}
-  {%- elif custom_schema_name == 'gold' -%}
+  {%- elif custom_schema_name == 'gold' -%}  
     {{ target_schema }}_gold  {# In non-production environments, combine target schema with 'gold' #}
   {%- elif custom_schema_name is not none -%}
     {{ target_schema }}_{{ custom_schema_name | trim }}
