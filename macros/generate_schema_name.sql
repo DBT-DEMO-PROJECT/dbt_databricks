d7a71743b8d19f8da4f19766b9dbda0678862398
Yethu Krishnan <yethukichu55@gmail.com>
2024-08-16 10:29:50 +0000

    updated the macro sql file

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,17 +1,9 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-  {%- set target_schema = target.schema -%}
+{% macro generate_schema_name(custom_schema_name, node) -%}
 
-  {%- if target.name == 'bronze' and custom_schema_name == 'silver' -%}
-    {{ 'silver' }}  {# In production, use only 'silver' schema #}
-  {%- elif target.name == 'bronze' and custom_schema_name == 'gold' -%}
-    {{ 'gold' }}  {# In production, use only 'gold' schema #}
-  {%- elif custom_schema_name == 'silver' -%}
-    {{ target_schema }}_silver  {# In non-production environments, combine target schema with 'silver' #}
-  {%- elif custom_schema_name == 'gold' -%}  
-    {{ target_schema }}_gold  {# In non-production environments, combine target schema with 'gold' #}
-  {%- elif custom_schema_name is not none -%}
-    {{ target_schema }}_{{ custom_schema_name | trim }}
-  {%- else -%}
-    {{ target_schema }}
-  {%- endif -%}
-{% endmacro %}
+    {%- if custom_schema_name in ['silver', 'gold'] -%}
+        {{ custom_schema_name }}
+    {%- else -%}
+        {{ target.schema }}_{{ custom_schema_name }}
+    {%- endif -%}
+
+{%- endmacro %}
