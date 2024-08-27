b5596535b7225c522c8f13db14f6c11a22174602
Yethu Krishnan <yethukichu55@gmail.com>
2024-08-27 21:31:18 +0530

    update to old code

---
+++ models/staging/stg_employee.sql
@@ -0,0 +1,15 @@
+-- models/staging/stg_employee.sql
+{{ config(schema='silver') }}
+
+with source as (
+  select * from {{ source('raw_data', 'dim_employee') }}
+)
+
+select
+  employee_id,
+  employee_name,
+  job_title,
+  hire_date,
+  department_id,
+  location
+from source
